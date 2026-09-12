a1602320af93c97df0b6e7eb8ac150e1cef4b198
0Y0X <154022703+0y0x@users.noreply.github.com>
2026-09-12 12:07:21 -0400

    Update arsenal.lua

--- games/arsenal.lua
+++ games/arsenal.lua
@@ -3513,12 +3513,12 @@ local function CreateModule(
 		local function SetAmmoValues()
 			for _, Ammo in ipairs(GetAllValues("Ammo")) do
 				SaveOriginal("Ammo", Ammo)
-				Ammo.Value = 299
+				Ammo.Value = 999
 			end
 
 			for _, StoredAmmo in ipairs(GetAllValues("StoredAmmo")) do
 				SaveOriginal("StoredAmmo", StoredAmmo)
-				StoredAmmo.Value = 499
+				StoredAmmo.Value = 299
 			end
 		end
 
